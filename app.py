b3fd33049a950f6ee00fd16dad8eea166617b079
Piyush S <singh.piyush575@gmail.com>
2024-06-22 22:40:00 +0530

    logger, exception and utills added

---
+++ app.py
@@ -0,0 +1,16 @@
+from helmet.logger import logging
+from helmet.exception import HelmetException
+import sys 
+
+
+logging.info("Welcome to this Project")
+
+("Welcome to this project")
+
+try:
+    a = 2 + '3'
+    print(a)
+except Exception as e:
+    logging.info(HelmetException(e, sys))
+
+    raise HelmetException(e, sys) from e   
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="helmet_detection",
+    version="0.0.0",
+    author="Piyush Singh",
+    author_email="singh.piyush575@gmail.com",
+    description="Industrial Protective Helmet Detection Project",
+    packages=find_packages()
+)
